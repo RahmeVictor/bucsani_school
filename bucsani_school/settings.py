aa7dec643338530cba322951273a68ba0e2ab62e
Rahme Victor <42947472+RahmeVictor@users.noreply.github.com>
2023-01-02 21:54:03 +0200

    hotfix

--- bucsani_school/settings.py
+++ bucsani_school/settings.py
@@ -143,8 +143,10 @@ CORS_ALLOW_ALL_ORIGINS = DEBUG
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+
 }
