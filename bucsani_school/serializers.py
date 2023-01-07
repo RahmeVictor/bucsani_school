f92960a1e5214b0724b8ca0117b28936249ec223
Rahme Victor <42947472+RahmeVictor@users.noreply.github.com>
2023-01-08 00:47:27 +0200

    test

--- bucsani_school/serializers.py
+++ bucsani_school/serializers.py
@@ -26,10 +26,9 @@ class PostTypeSerializer(ModelSerializer):
 
 class PostSerializer(ModelSerializer):
     images = SerializerMethodField(read_only=True)
-    type = PostTypeSerializer(required=False)
-
     # images = PostImageSerializer(many=True, read_only=True)
     files = PostFileSerializer(many=True, required=False)
+    type_id = IntegerField(required=False, write_only=True)
 
     class Meta:
         model = Post
@@ -43,18 +42,15 @@ class PostSerializer(ModelSerializer):
         return [PostFileSerializer(img, context=self.context).data['file'] for img in obj.files.all()]
 
     def create(self, validated_data):
-        type_data = validated_data.pop("type", None)
         post_type = None
-        if type_data:
-            post_type_pk: int = type_data.pop("id", None)
-            if post_type_pk:
-                post_type = PostType.objects.get(pk=post_type_pk)
+        post_type_pk: int = validated_data.pop("type_id", None)
+        if post_type_pk:
+            post_type = PostType.objects.get(pk=post_type_pk)
 
         post = self.Meta.model.objects.create(**validated_data, type=post_type)
         files = self.context['request'].FILES
         if files:
             try:
-                print(files.getlist('files'))
                 for f in files.getlist('files'):
                     post.files.get_or_create(file=f)
             except Exception as e:
@@ -62,17 +58,20 @@ class PostSerializer(ModelSerializer):
         return post
 
     def update(self, instance: Post, validated_data):
-        type_data = validated_data.pop("type", None)
         post_type = None
-        if type_data:
-            post_type_pk: int = type_data.pop("id", None)
-            if post_type_pk:
-                post_type = PostType.objects.get(pk=post_type_pk)
+        post_type_pk: int = validated_data.pop("type_id", None)
+        if post_type_pk:
+            post_type = PostType.objects.get(pk=post_type_pk)
 
         instance.type = post_type
         instance = super().update(instance, validated_data)
-        print(validated_data)
-        print(self.context['request'].FILES)
+        files = self.context['request'].FILES
+        if files:
+            try:
+                for f in files.getlist('files'):
+                    instance.files.get_or_create(file=f)
+            except Exception as e:
+                print(e)
         #PostType.objects.update_or_create(post=instance, pk=post_type_pk, defaults=post_type_data)
         return instance
 
